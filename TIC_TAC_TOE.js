e599d2d64796e66ca5214221e24fb6bda559c58d
Laasya deshkulkarni <152057054+ShivaLaasya@users.noreply.github.com>
2024-02-18 20:18:37 +0530

    Add files via upload

---
+++ TIC_TAC_TOE.js
@@ -0,0 +1,32 @@
+// let btns = document.querySelectorAll(".button");
+// let restartBtn = document.querySelector("#restart-btn");
+
+// let turn = "O";
+// btns.forEach(btn => {
+//     btn.addEventListener("click", ()=>{
+//         if (turn === "O") {
+//             btn.innerText = "O";
+//             turn = "X";
+//         }
+//         else {
+//             btn.innerText = "X";
+//             turn = "O";
+//         }
+//         btn.disabled=true;
+//     })
+// });
+
+let turnO = true;
+$(".button").click(function(){
+    if (turnO) {
+        $(this).text("O");
+        turnO = false;
+        $(this).prop("disabled",true);
+    }
+    else {
+        $(this).text("X");
+        turnO = true;
+        $(this).prop("disabled",true);
+    }
+});
+ 
