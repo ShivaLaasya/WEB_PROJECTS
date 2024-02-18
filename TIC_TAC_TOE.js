ad7e641692f389c179f65304e5eae3dc06fd1af2
Laasya deshkulkarni <152057054+ShivaLaasya@users.noreply.github.com>
2024-02-18 20:21:40 +0530

    Update TIC_TAC_TOE.js

--- TIC_TAC_TOE.js
+++ TIC_TAC_TOE.js
@@ -1,20 +1,20 @@
 // let btns = document.querySelectorAll(".button");
 // let restartBtn = document.querySelector("#restart-btn");
 
-let turn = "O";
-btns.forEach(btn => {
-    btn.addEventListener("click", ()=>{
-        if (turn === "O") {
-            btn.innerText = "O";
-            turn = "X";
-        }
-        else {
-            btn.innerText = "X";
-            turn = "O";
-        }
-        btn.disabled=true;
-    })
-});
+// let turn = "O";
+// btns.forEach(btn => {
+//     btn.addEventListener("click", ()=>{
+//         if (turn === "O") {
+//             btn.innerText = "O";
+//             turn = "X";
+//         }
+//         else {
+//             btn.innerText = "X";
+//             turn = "O";
+//         }
+//         btn.disabled=true;
+//     })
+// });
 
 let turnO = true;
 $(".button").click(function(){
